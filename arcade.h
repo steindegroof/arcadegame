0f1738564845a93405079869582b44e4835983ea
christophevvv <Christophe.Verdonck@gmail.com>
2015-03-19 14:25:06 +0100

    contracten toegevoegd

--- arcade.h
+++ arcade.h
@@ -170,6 +170,12 @@ public:
     * \param[OUT]   found           whether the player was found
     **/
     bool hasPlayer(const Player*);
+	/**
+    * \brief        returns the playpiece on a given coordinate.
+    * \param[IN]    x          the x coordinate of the playpiece
+	* \param[IN]    y          the y coordinate of the playpiece
+    * \param[OUT]   PlayPiece   pointer to the playpiece on the given position. If no playpiece was found returns nullptr.
+    **/
     PlayPiece* getPlayPiece(int x, int y) const;
     PlayPiece* getPlayPiece(std::pair<int, int>) const;
     /**
@@ -182,20 +188,63 @@ public:
     * \pre length > 0, width > 0
     */
     Field(std::string name, const int length, const int width);
+	/** 
+    * \brief Geeft terug of het veld goed geinitialiseerd is..
+	* \param[out]   succes	Geeft weer de klasse goed is geinitialiseerd.
+    **/
     bool properlyInitialized() const;
     /** 
-    * \brief verplaats een playpieve volgens de opgegeven move.
+    * \brief verplaats een playpiece volgens de opgegeven move.
     * \param[in]	move	De te maken move
-* \param[out]   succes	Geeft weer of de move gelukt is
+	* \param[out]   succes	Geeft weer of de move gelukt is
     **/
     bool doMove(const Move&);
+	/** 
+    * \brief Voeg een speler toe aan het veld
+    * \param[in]	player	De speler die je wil toevoegen
+	* \param[out]   succes	Geeft weer of het toevoegen gelukt is.
+	* \pre          De speler heeft geen negatieve coordinaten
+	* \post         De speler werd aan het veld toegevoegd indien succes true was.
+    **/
 	bool addPlayer(Player*);
+	/** 
+    * \brief Voeg een obstakel toe aan het veld
+    * \param[in]	obstacle	Het toe te voegen obstakel
+	* \param[out]   succes	Geeft weer of het toevoegen gelukt is.
+    * \pre          Het obstakel heeft geen negatieve coordinaten
+	* \post         Het obstakel werd aan het veld toegevoegd indien succes true was.
+    **/
 	bool addObstacle(Obstacle*);
+	/** 
+    * \brief Geeft de naam van het veld terug.
+	* \param[out]   name	de naam van het veld
+    **/
 	std::string getName();
+	/** 
+    * \brief Geeft de lengte van het veld terug.
+	* \param[out]   lengte	de lengte van het veld
+    **/
 	int getLength();
+	/** 
+    * \brief Geeft de breedte van het veld terug.
+	* \param[out]   breedte	de breedte van het veld
+    **/
 	int getWidth();
+	/** 
+    * \brief Geeft een pointer terug naar de speler met een bepaalde naam
+	* \param[in]	playername	De naam van de te zoeken speler
+	* \param[out]   player	het Player object met de gegeven naam. Of indien niet gevonden nullptr
+    **/
     Player* getPlayer(std::string playername) const;
+	/** 
+    * \brief Geeft vector met alle obstakels op het veld terug.
+	* \param[out]   vector<Obstacle*>	De vector met alle obstakels van het veld.
+    **/
 	std::vector<Obstacle*> getObstacles();
+	/** 
+    * \brief Geeft vector met alle spelers op het veld terug.
+	* \param[out]   vector<Player*>	De vector met alle players van het veld.
+    **/
 	std::vector<Player*> getPlayers();
 };
 
